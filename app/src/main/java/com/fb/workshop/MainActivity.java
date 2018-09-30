0da02b8f6c3c0e06f6beba6cb8218fc7d3fd977c
Suresh Michael Peiris <tsmpmail@gmail.com>
2018-09-30 16:57:19 +0530

    Facebook Dev SDK Setup

--- app/src/main/java/com/fb/workshop/MainActivity.java
+++ app/src/main/java/com/fb/workshop/MainActivity.java
@@ -3,11 +3,15 @@ package com.fb.workshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.AccountKit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
